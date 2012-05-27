008bcf5081187973607923dce9e111b1647d091f
Wim Looman <ghostunderscore@gmail.com>
2012-05-27 21:21:59 +1200

    And some more

---
+++ SubsonicApi/InternalExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RestSharp;
+using SubsonicApi.RestData;
+
+namespace SubsonicApi {
+    internal static class InternalExtensions {
+        internal static void ThrowOnErrors(this IRestResponse<SubsonicResponseData> response) {
+            if (response.ResponseStatus == ResponseStatus.Error) {
+                throw new ConnectionException(response.ErrorMessage, response.ErrorException);
+            }
+
+            if (!response.Data.IsOk) {
+                if (response.Data.Error != null) {
+                    throw response.Data.Error.ToException();
+                } else {
+                    throw new SubsonicServerException();
+                }
+            }
+        }
+
+    }
+}
